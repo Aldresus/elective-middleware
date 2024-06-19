0db3b4649fcf9914d66e369e49cefdf28d837601
Louis <louis.dumont@viacesi.fr>
2024-06-19 17:13:06 +0200

    add logger to log route

--- src/log/log.controller.ts
+++ src/log/log.controller.ts
@@ -7,6 +7,7 @@ import {
   Param,
   Delete,
   Query,
+  Request,
 } from '@nestjs/common';
 import { LogService } from './log.service';
 import { CreateLogDto } from './dto/create-log.dto';
@@ -19,13 +20,20 @@ import {
   ApiTags,
 } from '@nestjs/swagger';
 import { LogEntity } from './entities/log.entity';
+import { Roles } from 'src/role/role.decorator';
+import { Role } from 'src/role/role.enum';
+import { Utils } from 'src/utils/utils';
 
 @Controller('api/log')
 @ApiTags('log')
 export class LogController {
-  constructor(private readonly logService: LogService) {}
+  constructor(
+    private readonly logService: LogService,
+    private readonly utils: Utils,
+  ) {}
 
   @Get()
+  @Roles(Role.ADMIN, Role.COMMERCIAL, Role.TECHNICIAN)
   @ApiOperation({ summary: 'Get logs with optional filters' })
   @ApiCreatedResponse({ type: LogEntity, isArray: true })
   @ApiQuery({ name: 'id_log', required: false, type: String })
@@ -35,7 +43,17 @@ export class LogController {
     @Query('id_log') idLog: string,
     @Query('service') service: string,
     @Query('level') level: string,
+    @Request() req,
   ) {
+    const user = req.user;
+    const role = req.role;
+
+    this.utils.addLog({
+      service: 'LOG',
+      message: `get by ${user.id_user} (${role})`,
+      level: 'INFO',
+    } as CreateLogDto);
+
     return this.logService.findMany({
       id_log: idLog,
       service: service,
--- src/log/log.module.ts
+++ src/log/log.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { LogService } from './log.service';
 import { LogController } from './log.controller';
 import { HttpModule } from '@nestjs/axios';
+import { Utils } from 'src/utils/utils';
 
 @Module({
   controllers: [LogController],
   imports: [HttpModule],
-  providers: [LogService],
+  providers: [LogService, Utils],
 })
 export class LogModule {}
